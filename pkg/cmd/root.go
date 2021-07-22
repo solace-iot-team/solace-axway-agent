04d053b2a870f0daf3434bede8b250076710e63a
Jochen Traunecker <jochen.traunecker@gmail.com>
2021-07-22 16:34:39 +0200

    adjusted configuration properties

--- pkg/cmd/root.go
+++ pkg/cmd/root.go
@@ -22,10 +22,10 @@ func init() {
 	// The first parameter identifies the name of the yaml file that agent will look for to load the config
 	RootCmd = corecmd.NewRootCmd(
 		"solace_axway_agent",   // Name of the yaml file
-		"Solace Axway Agent", // Agent description
-		initConfig,               // Callback for initializing the agent config
-		listenToSubscriptions,    // Callback for executing the agent
-		corecfg.DiscoveryAgent,   // Agent Type (Discovery or Traceability)
+		"Solace Axway Agent",   // Agent description
+		initConfig,             // Callback for initializing the agent config
+		listenToSubscriptions,  // Callback for executing the agent
+		corecfg.DiscoveryAgent, // Agent Type (Discovery or Traceability)
 	)
 }
 
@@ -36,7 +36,7 @@ func run() error {
 }
 
 func listenToSubscriptions() error {
-	err:= connector.Initialize(gatewayConfig)
+	err := connector.Initialize(gatewayConfig)
 	if err != nil {
 		panic(err)
 	}
@@ -49,42 +49,42 @@ func listenToSubscriptions() error {
 }
 
 func handleUnsubscribeSubscription(subscription apic.Subscription) {
-	log.Debugf(" Handling unsubscribe for [Subscription:%s] ",subscription.GetName())
+	log.Debugf(" Handling unsubscribe for [Subscription:%s] ", subscription.GetName())
 	container, err := gateway.NewSubscriptionContainer(subscription)
 	if err != nil {
-		log.Errorf("Handling Unsubscribe for [Subscription:%s] was not successful. [%s]",subscription.GetName(),err.Error())
+		log.Errorf("Handling Unsubscribe for [Subscription:%s] was not successful. [%s]", subscription.GetName(), err.Error())
 		return
 	}
-	if (subscription.GetRemoteAPIID()=="" && container.Valid){
+	if subscription.GetRemoteAPIID() == "" && container.Valid {
 		//log.Info(" DUMP: ",container.Debug())
 		err := container.ProcessUnsubscribeSubscription()
 		if err != nil {
 			log.Error(err)
 			//TODO add some debug context-id to find log message
-			subscription.UpdateState(apic.SubscriptionFailedToUnsubscribe,"Failed to de-provision AsyncAPI")
+			subscription.UpdateState(apic.SubscriptionFailedToUnsubscribe, "Failed to de-provision AsyncAPI")
 		} else {
-			subscription.UpdateState(apic.SubscriptionUnsubscribed,"AsyncAPI de-provisioned at PubSub+ Broker")
+			subscription.UpdateState(apic.SubscriptionUnsubscribed, "AsyncAPI de-provisioned at PubSub+ Broker")
 		}
 	}
 
 }
 
 func handleApprovedSubscription(subscription apic.Subscription) {
-	log.Debugf(" Handling subscribe for [Subscription:%s] ",subscription.GetName())
+	log.Debugf(" Handling subscribe for [Subscription:%s] ", subscription.GetName())
 	container, err := gateway.NewSubscriptionContainer(subscription)
 	if err != nil {
-		log.Errorf("Handling subscribe for [Subscription:%s] was not successful. [%s]",subscription.GetName(),err.Error())
+		log.Errorf("Handling subscribe for [Subscription:%s] was not successful. [%s]", subscription.GetName(), err.Error())
 		return
 	}
-	if (subscription.GetRemoteAPIID()=="" && container.Valid){
+	if subscription.GetRemoteAPIID() == "" && container.Valid {
 		//log.Info(" DUMP: ",container.Debug())
 		err := container.ProcessSubscription()
 		if err != nil {
 			log.Error(err)
 			//TODO add some debug context-id to find log message
-			subscription.UpdateState(apic.SubscriptionFailedToSubscribe,"Failed to provision AsyncAPI")
+			subscription.UpdateState(apic.SubscriptionFailedToSubscribe, "Failed to provision AsyncAPI")
 		} else {
-			subscription.UpdateState(apic.SubscriptionActive,"AsyncAPI provisioned to PubSub+ Broker")
+			subscription.UpdateState(apic.SubscriptionActive, "AsyncAPI provisioned to PubSub+ Broker")
 		}
 	}
 }
@@ -95,11 +95,11 @@ func initConfig(centralConfig corecfg.CentralConfig) (interface{}, error) {
 	rootProps := RootCmd.GetProperties()
 	// Parse the config from bound properties and setup gateway config
 	gatewayConfig = &config.GatewayConfig{
-		ConnectorURL:           rootProps.StringPropertyValue("gateway-section.connector_url"),
-		ConnectorAdminUser:     rootProps.StringPropertyValue("gateway-section.connector_admin_user"),
-		ConnectorAdminPassword: rootProps.StringPropertyValue("gateway-section.connector_admin_password"),
-		ConnectorOrgUser:       rootProps.StringPropertyValue("gateway-section.connector_org_user"),
-		ConnectorOrgPassword:   rootProps.StringPropertyValue("gateway-section.connector_org_password"),
+		ConnectorURL:           rootProps.StringPropertyValue("gateway-section.connectorUrl"),
+		ConnectorAdminUser:     rootProps.StringPropertyValue("gateway-section.connectorAdminUser"),
+		ConnectorAdminPassword: rootProps.StringPropertyValue("gateway-section.connectorAdminPassword"),
+		ConnectorOrgUser:       rootProps.StringPropertyValue("gateway-section.connectorOrgUser"),
+		ConnectorOrgPassword:   rootProps.StringPropertyValue("gateway-section.connectorOrgPassword"),
 	}
 
 	agentConfig := config.AgentConfig{
